48399489ef4a3de83feeb85b005035ebcb20197e
Childelau <1299337429@qq.com>
2023-10-18 11:29:57 +0800

    111111

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import * as THREE  from 'three'
 
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import * as math from 'mathjs'
 
 // 创建一个场景
 var scene = new THREE.Scene();
@@ -36,38 +37,17 @@ controls.update()
 const cubeGeometry = new THREE.BoxGeometry(2, 2, 2);
 const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
 const cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
 cube.position.set(3.5, .5, .5)
-
-
-let points = [];
-points.push( new THREE.Vector3( 0, 1, 0 ) );
-points.push( new THREE.Vector3( 1, 1, 2.5 ) );
-
-let geometry = new THREE.BufferGeometry().setFromPoints( points );
-let material = new THREE.LineBasicMaterial({
-    color:0xff0000
-});
-const line = new THREE.Line(geometry, material)
-scene.add(line)
-
-
-const pointer1 = cube.localToWorld(new THREE.Vector3(0.5, 0.5, 0.5))
-console.log(pointer1);
-// 将网格对象添加到场景
-scene.add(cube);
+scene.add(cube)
 
 
 
-const ray2 = new THREE.Vector3()
-ray2.subVectors(new THREE.Vector3(2, 1, 1), new THREE.Vector3(0, 1, 3)).normalize()
-console.log(ray2);
+function handleAfterRender() {
+    console.log('The cube has been rendered!');
+}
 
-const raycaster = new THREE.Raycaster(new THREE.Vector3(0, 1, 3), ray2);
-raycaster.params.Line.threshold = 0.05 ;
-const intersects = raycaster.intersectObject( line )
-console.log(intersects);
-
+cube.onAfterRender = handleAfterRender;
+// cube.onAfterRender = function() {};
 
 
 
