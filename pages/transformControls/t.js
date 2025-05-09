7a1094f4914775def4a206aa43dd140e88fb9671
liuwei <1299337429@qq.com>
2025-05-09 17:08:12 +0800

    111

--- pages/transformControls/t.js
+++ pages/transformControls/t.js
@@ -7,16 +7,15 @@ import { BoxHelper } from 'three';
 import { TransformControls } from 'three/examples/jsm/controls/TransformControls.js';
 
 // 创建一个场景
-var scene = new THREE.Scene();
+const scene = new THREE.Scene();
 
 // 创建一个相机
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 camera.position.set(0, 10, 10);
-
-// 创建一个渲染器
-var renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const renderer = new THREE.WebGLRenderer({antialias: true});
+const a = document.getElementById('a');
+renderer.setSize(a.clientWidth, a.clientHeight);
+a.appendChild(renderer.domElement);
 
 
 const axesHelper = new THREE.AxesHelper( 5 );
@@ -24,9 +23,23 @@ scene.add( axesHelper );
 const gridHelper = new THREE.GridHelper( 20, 20 );
 scene.add( gridHelper );
 
-//
 const controls = new OrbitControls(camera, renderer.domElement )
 controls.update()
+
+
+
+
+const camera1 = new THREE.OrthographicCamera(-20, 20, 20, -20, 0, 100);
+camera1.position.set(0, 30, 10);
+const renderer1 = new THREE.WebGLRenderer({antialias: true});
+const b = document.getElementById('b');
+renderer1.setSize(b.clientWidth, b.clientHeight);
+b.appendChild(renderer1.domElement);
+const controls1 = new OrbitControls(camera1, renderer1.domElement )
+controls1.update()
+
+
+
 
 
 const cubeGeometry = new THREE.BoxGeometry(10,5,1)
@@ -35,13 +48,38 @@ const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
 cube.position.y = 2.5
 
 
-const cube1 = new THREE.Mesh(
-    new THREE.BoxGeometry(2, 1, .6),
-    new THREE.MeshBasicMaterial({color: 0xffff00})
-)
-// cube.add(cube1)
 
 scene.add(cube)
+
+const transformControls = new TransformControls(camera, renderer.domElement);
+transformControls.setSize(1);
+scene.add(transformControls);
+
+transformControls.addEventListener('dragging-changed', function (event) {
+    controls.enabled = !event.value;
+})
+transformControls.attach(cube);
+
+const transformControls1 = new TransformControls(camera1, renderer1.domElement);
+transformControls1.setSize(1);
+scene.add(transformControls1);
+transformControls1.addEventListener('dragging-changed', function (event) {
+    controls1.enabled = !event.value;
+})
+transformControls1.attach(cube);
+
+
+let activeTransform = transformControls; // 默认激活div 'a'的控件
+transformControls1.addEventListener('mouseDown', () => {
+    activeTransform = transformControls1;
+    transformControls1.enabled = true;
+    transformControls.enabled = false; // 禁用另一个
+});
+transformControls.addEventListener('mouseDown', () => {
+    activeTransform = transformControls;
+    transformControls1.enabled = false;
+    transformControls.enabled = true;
+});
 
 
 
@@ -50,5 +88,6 @@ function animate() {
     requestAnimationFrame(animate);
     controls.update()
     renderer.render(scene, camera);
+    renderer1.render(scene, camera1);
 }
 animate();
